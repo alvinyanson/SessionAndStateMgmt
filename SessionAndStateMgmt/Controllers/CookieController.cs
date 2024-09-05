3bc6f15f1053cc1f9fb2b9796d662d0283a9eb77
alvinyanson <a.yanson@arcanys.com>
2024-09-05 12:26:12 +0800

    Add project files.

---
+++ SessionAndStateMgmt/Controllers/CookieController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace SessionAndStateMgmt.Controllers
+{
+    public class CookieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Name, "Alvin"),
+                new Claim(ClaimTypes.MobilePhone, "1234567890"),
+                new Claim(ClaimTypes.Email, "nV9oJ@example.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            var user = HttpContext.User;
+
+            var result = new
+            {
+                Id = user.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                Name = user.FindFirst(ClaimTypes.Name)?.Value,
+                Phone = user.FindFirst(ClaimTypes.MobilePhone)?.Value,
+                Email = user.FindFirst(ClaimTypes.Email)?.Value,
+                Role = user.FindFirst(ClaimTypes.Role)?.Value
+            };
+
+            return View(result);
+        }
+    }
+}
---
+++ SessionAndStateMgmt/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie(options =>
+{
+    options.LoginPath = "/";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
