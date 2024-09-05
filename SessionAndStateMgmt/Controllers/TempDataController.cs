c609c95d2e968d3526484a1726220e0c105db0b7
alvinyanson <a.yanson@arcanys.com>
2024-09-05 14:07:11 +0800

    feat: temp data state management

---
+++ SessionAndStateMgmt/Controllers/TempDataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SessionAndStateMgmt.Models;
+
+namespace SessionAndStateMgmt.Controllers
+{
+    public class TempDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CommentForm request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            TempData["SuccessMsg"] = "Form submitted successfully";
+
+            ModelState.Clear();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SessionAndStateMgmt/Models/CommentForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SessionAndStateMgmt.Models
+{
+    public class CommentForm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+    }
+}
